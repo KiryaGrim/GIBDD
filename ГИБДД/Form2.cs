219e3b4d9d0130cefb6e645273d38641a36330e9
Vladimir <poeluev.vladimir@gmail.com>
2024-03-30 15:05:52 +0300

    Form2

---
+++ ГИБДД/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ГИБДД
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Открываем диалог выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPG Files|*.jpg|PNG Files|*.png";
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Выберите фотографию водителя";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Проверяем, соответствует ли выбранный файл заданным условиям
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                Bitmap bitmap = new Bitmap(openFileDialog.FileName);
+                /*if (4 * bitmap.Width != 3 * bitmap.Height || bitmap.Width > 2000 || bitmap.Height > 2000)
+                {
+                    MessageBox.Show("Выбранный файл не соответствует заданным параметрам", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    // Если все условия выполнены, прикрепляем изображение к кнопке*/
+                    pictureBox1.Image = bitmap;
+                    button1.Controls.Add(pictureBox1);
+                //}
+            }
+        }
+    }
+}
